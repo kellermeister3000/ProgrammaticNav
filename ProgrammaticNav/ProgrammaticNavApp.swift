e499c362cf99452676eb5ecb1277dc6442a4f612
Philip Keller <philipjoelkeller@gmail.com>
2023-03-07 20:10:09 -0800

    Initial Commit

---
+++ ProgrammaticNav/ProgrammaticNavApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgrammaticNavApp.swift
+//  ProgrammaticNav
+//
+//  Created by Philip Keller on 3/7/23.
+//
+
+import SwiftUI
+
+@main
+struct ProgrammaticNavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
